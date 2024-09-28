0a3b0cf91cfad3b48fba0854e2f22821c1eb714b
toran billups <toranb@gmail.com>
2024-09-28 07:57:49 -0500

    configure pgvector

--- config/dev.exs
+++ config/dev.exs
@@ -8,7 +8,8 @@ config :example, Example.Repo,
   database: "example_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 10,
+  types: Example.PostgrexTypes
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
---
+++ lib/postgrex_types.ex
@@ -0,0 +1,1 @@
+Postgrex.Types.define(Example.PostgrexTypes, Pgvector.extensions() ++ Ecto.Adapters.Postgres.extensions(), [])
